7f17b5657c679d0ecf9f24bce75083cf8f99d649
luxusproblem <dominik.leipelt@googlemail.com>
2020-05-10 16:57:32 +0200

    remove sample

--- FXQuick/src/main/java/fxQuick/FontInitializer.java
+++ FXQuick/src/main/java/fxQuick/FontInitializer.java
@@ -8,9 +8,9 @@ public class FontInitializer {
     public static Font lineAwesome;
 
     public static void init() {
-        Font.loadFont(FontInitializer.class.getClassLoader().getResourceAsStream("materialReg.ttf"), 16);
-        Font.loadFont(FontInitializer.class.getClassLoader().getResourceAsStream("matOutline.ttf"), 16);
-        lineAwesome=  Font.loadFont(FontInitializer.class.getClassLoader().getResourceAsStream("la-regular-400.ttf"),16);
+        Font.loadFont(FontInitializer.class.getClassLoader().getResourceAsStream("fxQuickFonts/materialReg.ttf"), 16);
+        Font.loadFont(FontInitializer.class.getClassLoader().getResourceAsStream("fxQuickFonts/matOutline.ttf"), 16);
+        lineAwesome=  Font.loadFont(FontInitializer.class.getClassLoader().getResourceAsStream("fxQuickFonts/la-regular-400.ttf"),16);
         setInitalized(true);
     }
 
